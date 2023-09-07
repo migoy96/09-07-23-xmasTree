65405b2e2fa9af0eda86e7079bb5f7b494d370f2
migoy96 <142022968+migoy96@users.noreply.github.com>
2023-09-07 16:45:22 +0800

    Add files via upload
    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let star = "*"
+
+for (let i = 0; i < 2; i++){
+    document.getElementById("tree_star").innerHTML 
+    += star + "<br>";
+    star = star + "*";
+}
+
+let starTwo = "*0*"
+
+for (let i = 0; i < 16; i++){
+    document.getElementById("tree_star").innerHTML 
+    += starTwo + "<br>";
+    starTwo = starTwo + "0*";
+}
+
+function treeLights(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let rGb = "rgb("+ red +", "+ green +", "+ blue +")";
+
+    document.getElementById("rgb_code").textContent = rGb;
+
+    document.getElementById("rgb_code").style.color = "white";
+
+    document.getElementById("tree_star").style.color = rGb;
+}
+
+function on(){
+    start = setInterval(treeLights, 500);
+}
+
+function off(){
+    clearInterval(start);
+}
